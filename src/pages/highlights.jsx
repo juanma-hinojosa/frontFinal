e4bd2ed304aadd3d359652f33b969ece3cc6abb4
Juan Hinojosa <90215736+juanma-hinojosa@users.noreply.github.com>
2023-03-03 17:38:23 -0300

    Update highlights.jsx

--- src/pages/highlights.jsx
+++ src/pages/highlights.jsx
@@ -73,8 +73,8 @@ const Highlights = () => {
                         </p>
                     </div>
                     <div className="video">
-                        <iframe src="https://www.youtube.com/embed/cqsuf8VSxuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
+                       // <iframe src="https://www.youtube.com/embed/cqsuf8VSxuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/sSk9hrwpkZM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
